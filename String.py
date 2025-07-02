7aa560fca85b7fa81b481e51c0eb350ac0f4e644
MokhamadBayuAdi <mokhamadbayuadisaputra@gmail.com>
2025-07-02 08:57:31 +0700

    Ini adalah string di dalam python

---
+++ String.py
@@ -0,0 +1,15 @@
+# ini adalah string yaitu type data untuk mengelolah sekumpulan carakter yang berupa huruf, angka, dan simbol 
+# contoh
+
+nama = "Mokhamad Bayu Adisaputra"
+print(nama)
+
+# penggabungan string
+# contoh 
+
+namaAwal = "Mokhamad"
+namaTengah = "Bayu"
+namaAkhir = "Adisaputra"
+
+gabungNama = namaAwal + " " + namaTengah + " " + namaAkhir 
+print(gabungNama)
